38421bfec311e34cf3adc1157a21177cbf2c2877
sumudu-k <lakshankulathunga21@gmail.com>
2025-07-09 21:18:05 +0530

    added composer

--- db.php
+++ db.php
@@ -1,8 +1,14 @@
 <?php
-$host = "localhost";
-$dbname = "sms";
-$username = "root";
-$password = "";
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
 
 try {
     $conn = new mysqli($host, $username, $password, $dbname);
